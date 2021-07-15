e6f247197eb8ac747457b509dd1c6c38d8a8ddf7
Neeraj <nkthecoder@gmail.com>
2021-07-15 17:45:26 +0530

    Variables in javascript

---
+++ JavaScript/Variables.js
@@ -0,0 +1,12 @@
+let a = 45;
+const b = 45;
+let animal = "Dogs"
+console.log(`I have ${a+b} no of ${animal}`)
+    // using math object
+console.log(Math.sqrt(100));
+console.log(Math.floor((Math.random()) * 6));
+//Using some operator in javascript
+let c = 23;
+let d = 26;
+console.log(c === d);
+console.log(c <= d);
---
+++ JavaScript/dist/Variables.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var a = 45;
+var b = 45;
+var animal = "Dogs";
+console.log("I have ".concat(a + b, " no of ").concat(animal)); // using math object
+
+console.log(Math.sqrt(100));
+console.log(Math.floor(Math.random() * 6)); //Using some operator in javascript
+
+var c = 23;
+var d = 26;
+console.log(c === d);
+console.log(c <= d);
+//# sourceMappingURL=Variables.dev.js.map
