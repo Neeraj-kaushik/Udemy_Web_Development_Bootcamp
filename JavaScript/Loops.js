4c5e8f71b1a13878156c56dc16afd8c33d8f01f7
Neeraj <nkthecoder@gmail.com>
2021-07-16 22:28:30 +0530

    For loop in javascript

---
+++ JavaScript/Loops.js
@@ -0,0 +1,11 @@
+// Creating and array and using for loop to iterate over it.
+let li = ['lion', 12, 'tiger', 6, 'elephant', 50, 'whales', 70];
+for (let i = 0; i <= li.length; i++) {
+    console.log(li[i])
+}
+
+//using other propety while using for loop on array.
+li = ['lion', 'tiger', 'elephant', 'whale'];
+for (i = 0; i <= li.length; i++) {
+    console.log(li[i].toUpperCase())
+}
