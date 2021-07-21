cb1f25e3145c1a68af4c076c9a558f048962599e
Neeraj <nkthecoder@gmail.com>
2021-07-21 11:54:10 +0530

    Attribute Javascript

---
+++ DOM/Attribute.js
@@ -0,0 +1,4 @@
+// Here we are using attribute to select and 
+const value = document.querySelector('img')
+value.setAttribute('src', 'C:/Users/Neeraj Kaushik/Desktop/Udemy/BasketBall/Place Your Image Here (Double Click to Edit) (2).png')
+value.setAttribute('alt', 'Error!!!')
